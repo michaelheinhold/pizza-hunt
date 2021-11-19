4c94582a60afb8fbfaafe32a7d400748bafb557b
DerVerruckte <michael.heinhold@gmail.com>
2021-11-19 11:23:08 -0700

    comments and replies

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,6 +5,12 @@ const pizzaController = {
   //get all pizza
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
     .then(dbPizzaData => res.json(dbPizzaData))
     .catch(err => {
       console.log(err);
@@ -14,6 +20,11 @@ const pizzaController = {
   //find one pizza
   getPizzaById({ params }, res){
     Pizza.findOne({ _id: params.id })
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
     .then(dbPizzaData => {
       //if no pizza is found send 404
       if (!dbPizzaData) {
